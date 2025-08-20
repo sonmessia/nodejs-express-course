3c33ee1fd5c133cf65f559443115ca795a70c1bc
Hoang Son <sonmessia123@gmail.com>
2025-08-20 23:44:16 +0700

    Refactor server setup and implement event emitter; add async file reading with error handling

---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,5 @@
+setInterval(() => {
+    console.log('Interval running');
+}, 1000);
+
+console.log('Started a first  task');
--- app.js
+++ app.js
@@ -1,23 +1,13 @@
-// npm - global command, comes with node
-// npx - package runner, comes with npm
-
-// local dependencies - packages that are only needed for a specific project
-// npm install <package_name>
-
-// global dependencies - packages that are needed for multiple projects
-// npm install -g <package_name>
-
-// package.json - file that keeps track of all dependencies for a project
-// created when running npm init, npm init -y, or npm install
-// includes metadata about the project and its dependencies
-// package-lock.json - file that locks the versions of dependencies for a project
+const http = require('http');
 
 
-const _ = require('lodash');
+const server = http.createServer();
 
-const items = [1, [2, [3, 4, [5]]]];
+server.on('request', (req, res) => {
+    console.log('Request received', req.url);
+    res.end('Hello World');
+});
 
-const newItems = _.flattenDeep(items);
-
-console.log(newItems);
-console.log('Hello World!');
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
